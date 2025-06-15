560b5a49f8a120e18c04a8459fb84f5b7a651b13
wendel <wendelsil@gmail.com>
2025-06-15 08:09:57 -0300

    Initial commit

---
+++ src/main/java/com/rfinformatica/ordem/servico/OrdemServicoApplication.java
@@ -0,0 +1,13 @@
+package com.rfinformatica.ordem.servico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdemServicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdemServicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rfinformatica/ordem/servico/OrdemServicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rfinformatica.ordem.servico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdemServicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
